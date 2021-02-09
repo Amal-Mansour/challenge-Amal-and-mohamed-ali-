be57fbde0422299c49dae0b55cba74b95d6ec96b
Amal-Mansour <amal.mans556@gmail.com>
2021-02-09 17:35:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Chrono from './Component/Chrono'
+
+function App() {
+  return (
+    <div className="App" >
+      <Chrono/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Chrono.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import {Button} from 'react-bootstrap'
+export default class chrono extends Component {
+    state = {
+        count: 0,
+        show: true,
+
+    }
+
+
+    handelClickStart = () => {
+        
+        this.interval = setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+
+            })
+        }, 1000)
+        this.setState({
+            show : !this.state.show
+        })
+    }
+
+    handelClickStop = () => {
+        clearInterval(this.interval)
+
+        this.setState({
+            show : !this.state.show
+        })
+
+    }
+  
+    handelClickReset =()=>{
+        this.setState({
+            count: 0
+        })
+        clearInterval(this.interval)
+    }
+
+    render() {
+
+        return (
+            <div style={{width:"400px" , height:"300px",backgroundColor:"black", display:"flex" , flexDirection:"column" , justifyContent:"center", alignItems:"center" }}>
+                <h1 style={{fontSize: "50px",color:"white"}}>{this.state.count}</h1>
+                {/* onclick */}
+                <div  style={{display:"flex" , justifyContent:"space-between"}}>
+                {this.state.show ?  <Button variant="success" onClick={this.handelClickStart}>Start</Button> :<Button  variant="warning" onClick={this.handelClickStop}>Stop</Button>}
+                <Button style={{marginLeft:"40px"}}  variant="danger" onClick={this.handelClickReset}>Reset</Button>
+                </div>
+
+            </div>
+        )
+    }
+}
